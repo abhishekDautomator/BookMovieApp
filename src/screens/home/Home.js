736d4eb9108c700f29ae149530e4bfb7092b4817
Abhishek Narayan <narayan.abhishek.28@gmail.com>
2021-10-17 12:14:34 +0530

    Released movies section addition

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,17 +1,25 @@
-import React,{ useState} from "react";
+import React, { useState } from "react";
 import Header from "../../common/header/Header";
 import "./Home.css";
-import { GridList, GridListTile, GridListTileBar} from "@material-ui/core";
+import { GridList, GridListTile, GridListTileBar } from "@material-ui/core";
 
-const images = [
-    { thumbnail: { uri: 'https://lorempixel.com/200/200/animals',name:'animals'}},
-    { thumbnail: { uri: 'https://lorempixel.com/200/200/city'  ,name:'city'}},
-    { thumbnail: { uri: 'https://lorempixel.com/200/200/city'  ,name:'city'}},
-    { thumbnail: { uri: 'https://lorempixel.com/200/200/city'  ,name:'city'}},
-    { thumbnail: { uri: 'https://lorempixel.com/200/200/nature'  ,name:'nature'} },
-    { thumbnail: { uri: 'https://lorempixel.com/200/200/cats'  ,name:'cats'} },
-    { thumbnail: { uri: 'https://lorempixel.com/200/200/cats'  ,name:'cats'} },
-    { thumbnail: { uri: 'https://lorempixel.com/200/200/cats'  ,name:'cats'} },
+const upcomingMovies = [
+  { thumbnail: { uri: "https://lorempixel.com/200/200/animals",name: "animals" } },
+  { thumbnail: { uri: "https://lorempixel.com/200/200/city", name: "city" } },
+  { thumbnail: { uri: "https://lorempixel.com/200/200/city", name: "city" } },
+  { thumbnail: { uri: "https://lorempixel.com/200/200/city", name: "city" } },
+  { thumbnail: { uri: "https://lorempixel.com/200/200/nature", name: "nature" } },
+  { thumbnail: { uri: "https://lorempixel.com/200/200/cats", name: "cats" } },
+  { thumbnail: { uri: "https://lorempixel.com/200/200/cats", name: "cats" } },
+  { thumbnail: { uri: "https://lorempixel.com/200/200/cats", name: "cats" } },
+];
+
+const releasedMovies = [
+    { thumbnail: { uri: "https://lorempixel.com/200/200/animals", name: "animals", releaseDate: "Fri Aug 11 2017"} },
+    { thumbnail: { uri: "https://lorempixel.com/200/200/city", name: "city", releaseDate: "Fri Aug 11 2017"} },
+    { thumbnail: { uri: "https://lorempixel.com/200/200/city", name: "city", releaseDate: "Fri Aug 11 2017"} },
+    { thumbnail: { uri: "https://lorempixel.com/200/200/city", name: "city", releaseDate: "Fri Aug 11 2017"} },
+    { thumbnail: { uri: "https://lorempixel.com/200/200/nature", name: "nature", releaseDate: "Fri Aug 11 2017"} }
   ];
 
 export default function Home() {
@@ -19,18 +27,34 @@ export default function Home() {
     <>
       <Header></Header>
       <header className="head">Upcoming Movies</header>
-        <GridList cellHeight={180} cols={6} >
-            <div className="grid_scroll" style={{width: '100%'}}>
-            {images.map((image) => (
-            <GridListTile>
-                <img src={image.thumbnail.uri} />
-                <GridListTileBar
-                    title={ image.thumbnail.name}
-                />
+      <GridList cellHeight={180} cols={6}>
+        <div className="grid_scroll" style={{ width: "100%" }}>
+          {upcomingMovies.map((image) => (
+            <GridListTile className="tiles_scroll">
+              <img src={image.thumbnail.uri} />
+              <GridListTileBar title={image.thumbnail.name} />
             </GridListTile>
+          ))}
+        </div>
+      </GridList>
+      <div className="container" style={{ width: "100%" }}>
+        <div className="column1">
+          <GridList cellHeight={350} cols={4}>
+            {releasedMovies.map((image) => (
+              <GridListTile className="tiles">
+                <img src={image.thumbnail.uri} />
+                <GridListTileBar 
+                    title={image.thumbnail.name}
+                    subtitle={image.thumbnail.releaseDate} 
+                />
+              </GridListTile>
             ))}
-            </div>
-        </GridList>
+          </GridList>
+        </div>
+        <div className="column2">
+          <h1>Hello</h1>
+        </div>
+      </div>
     </>
   );
 }
